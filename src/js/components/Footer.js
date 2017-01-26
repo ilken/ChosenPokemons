915e5fcc55585833c92f2ade8a88def44a5a14e1
Ilken Bahcecioglu <ilken.bahcecioglu@playtech.com>
2017-01-26 10:38:41 +0000

    Added Logo

--- src/js/components/Footer.js
+++ src/js/components/Footer.js
@@ -7,7 +7,7 @@ export default class Footer extends React.Component {
 				<div className="row">
 					<div className="col-md-12 col-sm-12 col-xs-12">
 						Pokemon icons by
-						<a href="http://theartificial.nl/"> The Artificial</a>
+						<a href="http://theartificial.nl/" target="_blank"> The Artificial</a>
 					</div>
 				</div>
 			</footer>
--- src/js/components/Header.js
+++ src/js/components/Header.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 export default class Header extends React.Component {
 	render () {
+		const pokeball = "/img/pokeball.svg";
+
 		return (
             <nav className="navbar navbar-inverse navbar-fixed-top top-nav-collapse" role="navigation">
                 <div className="container">
                     <div className="navbar-header">
                         <a className="navbar-brand" target="_blank" href="#">
-							<img className="img-responsive" src="" alt="PokeBall"/> Chosen Pokemons
+							<img className="img-responsive" src={pokeball} alt="PokeBall"/> London High IV Pokemons
 						</a>
                     </div>
                 </div>
