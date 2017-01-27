be64ec98df1820b5ba738e74ae839474d4c6225e
Ilken Bahcecioglu <ilken.bahcecioglu@playtech.com>
2017-01-27 16:48:50 +0000

    Improved info window

--- src/js/components/InfoWindow.js
+++ src/js/components/InfoWindow.js
@@ -36,9 +36,26 @@ export default class InfoWindow extends React.Component {
 	}
 
 	openInfoWindow(){
+		let pokemon = this.props.infoWindowData.pokemon;
+		let content = this.buildInfoContent(pokemon);
+
 		this.props.map.setCenter(this.props.infoWindowData.marker.getPosition());
-		this.state.infoWindow.setContent("HELLOOOOOO!");
+		this.state.infoWindow.setContent(content);
 		this.state.infoWindow.open(this.props.map, this.props.infoWindowData.marker);
+	}
+
+	buildInfoContent(pokemon){
+		let despawn = new Date(pokemon.expires * 1000);
+		let hours = despawn.getHours();
+		let minutes = "0" + despawn.getMinutes();
+		let seconds = "0" + despawn.getSeconds();
+		let formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+
+		return '<div class="infoWindow text-center">'
+				+ '<span class="name">' + pokemon.name + ' ' + pokemon.iv +'%</span> <br>'
+				+ '<span class="stats">Atk:' + pokemon.attack + ' | Def:' + pokemon.defence + ' | Sta:' + pokemon.stamina + '</span> <br>'
+				+ '<span class="expires">Expires: ' + formattedTime + '</span> <br>'
+				+ '</div>';
 	}
 
 	render () {
