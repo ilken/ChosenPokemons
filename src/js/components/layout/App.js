07627e09cf569752e1b9f6a9b334e9e884a38583
Ilken Bahcecioglu <ilken.bahcecioglu@playtech.com>
2017-02-10 10:45:23 +0000

    Added extra eslint rules

--- src/js/components/layout/App.js
+++ src/js/components/layout/App.js
@@ -26,31 +26,30 @@ export default class App extends React.Component {
 		if (this.props.loading) {
 			return (<LoadingScreen loadingScreen={this.props.loadingScreen}/>);
 		}
-		else {
-			const PokemonComponents = this.props.pokemons.map((pokemon) => {
-				return <Pokemon key={pokemon.name + pokemon.lat + pokemon.lon} data={pokemon} dispatch={this.props.dispatch}/>;
-			});
 
-			return (
-				<div className="container">
-					<div className="row">
-						<div className="col-md-12 col-sm-12 col-xs-12">
-							<PokeMap
-								dispatch={this.props.dispatch}
-								pokemons={this.props.pokemons}
-								map={this.props.map} />
-						</div>
-						<div className="col-md-12 col-sm-12 col-xs-12">
-							<IVSlider
-								dispatch={this.props.dispatch}
-								slider={this.props.slider} />
-						</div>
-						<div className="col-md-12 col-sm-12 col-xs-12">
-							{PokemonComponents}
-						</div>
+		const PokemonComponents = this.props.pokemons.map((pokemon) => {
+			return <Pokemon key={pokemon.name + pokemon.lat + pokemon.lon} data={pokemon} dispatch={this.props.dispatch}/>;
+		});
+
+		return (
+			<div className="container">
+				<div className="row">
+					<div className="col-md-12 col-sm-12 col-xs-12">
+						<PokeMap
+							dispatch={this.props.dispatch}
+							pokemons={this.props.pokemons}
+							map={this.props.map} />
+					</div>
+					<div className="col-md-12 col-sm-12 col-xs-12">
+						<IVSlider
+							dispatch={this.props.dispatch}
+							slider={this.props.slider} />
+					</div>
+					<div className="col-md-12 col-sm-12 col-xs-12">
+						{PokemonComponents}
 					</div>
 				</div>
-			);
-		}
+			</div>
+		);
 	}
 }
