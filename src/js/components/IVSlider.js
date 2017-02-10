ff274b40d82caff65aa0dff5785610937231756f
Ilken Bahcecioglu <ilken.bahcecioglu@playtech.com>
2017-02-10 14:19:17 +0000

    New range slider

--- src/js/components/IVSlider.js
+++ src/js/components/IVSlider.js
@@ -1,25 +1,33 @@
 import React from 'react';
-import Slider from 'react-rangeslider';
+import InputRange from 'react-input-range';
 import { updateSliderValue } from '../actions/sliderActions';
 
 export default class IVSlider extends React.Component {
+	constructor (props) {
+		super(props);
+
+		this.state = {
+			value: this.props.slider.value
+		};
+	}
+
 	handleChange (value) {
 		this.props.dispatch(updateSliderValue(value));
 	}
 
 	render () {
-		const labels = { 80: 'Low IV', 100: 'High IV' };
 		return (
 			<div className="slider">
-				<Slider
-					min={80}
-					max={100}
-					step={1}
-					labels={labels}
-					value={this.props.slider.value}
-					onChange={this.handleChange.bind(this)} />
+				<InputRange
+					ariaLabelledby=""
+					ariaControls=""
+			        maxValue={100}
+			        minValue={80}
+			        value={this.state.value}
+			        onChange={value => this.setState({ value })}
+			        onChangeComplete={this.handleChange.bind(this)} />
 				<div className="sliderValue text-center">
-					<h2>Min IV%: {this.props.slider.value} </h2>
+					<h2 className="sliderText">Min IV: {this.state.value}% </h2>
 				</div>
 			</div>
 		);
