4c185f49dca60eeac4bb5c7e688806b18d3f1848
ILKEN BAHCECIOGLU <ILKEN BAHCECIOGLU>
2017-02-19 10:09:33 +0000

    Switch button icons

--- src/js/components/PokeSwitch.js
+++ src/js/components/PokeSwitch.js
@@ -15,16 +15,24 @@ export default class Layout extends React.Component {
 		return (
             <div className="pokeSwitch row">
                 <div className="col-md-3 col-sm-3 col-xs-6">
-					<button type="button" className={'gen1 btn btn-block ' + gen1Class} onClick={this.handleClick.bind(this, 'gen1')}>Gen1</button>
+					<button type="button" className={'gen1 btn btn-block ' + gen1Class} onClick={this.handleClick.bind(this, 'gen1')}>
+						<i className="fa fa-hand-pointer-o" aria-hidden="true"/> Gen1
+					</button>
 				</div>
                 <div className="col-md-3 col-sm-3 col-xs-6">
-					<button type="button" className={'gen2 btn btn-block ' + gen2Class} onClick={this.handleClick.bind(this, 'gen2')}>Gen2</button>
+					<button type="button" className={'gen2 btn btn-block ' + gen2Class} onClick={this.handleClick.bind(this, 'gen2')}>
+						<i className="fa fa-hand-peace-o" aria-hidden="true"/> Gen2
+					</button>
 				</div>
                 <div className="col-md-3 col-sm-3 col-xs-6">
-					<button type="button" className={'rare btn btn-block ' + rareClass} onClick={this.handleClick.bind(this, 'rare')}>Rare</button>
+					<button type="button" className={'rare btn btn-block ' + rareClass} onClick={this.handleClick.bind(this, 'rare')}>
+						<i className="fa fa-diamond" aria-hidden="true"/> Rare
+					</button>
 				</div>
                 <div className="col-md-3 col-sm-3 col-xs-6">
-					<button type="button" className={'nearMe btn btn-block ' + nearMeClass} onClick={this.handleClick.bind(this, 'nearMe')}>Near Me</button>
+					<button type="button" className={'nearMe btn btn-block ' + nearMeClass} onClick={this.handleClick.bind(this, 'nearMe')}>
+						<i className="fa fa-crosshairs" aria-hidden="true"/> Near Me
+					</button>
 				</div>
             </div>
 		);
